3f7b12de49c94c58e579c8d19687f15435de8f08
KELVIN_MARCELLO <marcelo.kelvinn@gmail.com>
2023-05-13 23:35:01 +0700

    commit

---
+++ kelvin-restaurant-management-master/README.md
@@ -0,0 +1,1 @@
+# kelvin-restaurant-management
---
+++ kelvin-restaurant-management-master/server/models/item.js
@@ -0,0 +1,70 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Item.belongsTo(models.User)
+    }
+  }
+  Item.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty : {
+          message: "Name cannot be null"
+        }
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty : {
+          message: "Category cannot be null"
+        }
+      }
+    },
+    price: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty : {
+          message: "Price cannot be null"
+        },
+        isNumeric: {
+          message: "Price must be a number."
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty : {
+          message: "Stock cannot be null"
+        },
+        isNumeric: {
+          message: "Stock must be a number."
+        }
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty : {
+          message: "Image cannot be null"
+        }
+      }
+    },
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Item',
+  });
+  return Item;
+};
